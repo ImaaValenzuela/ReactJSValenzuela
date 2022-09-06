66dc9ee917efbc2966eb5e285381fc8abaa6d40e
enltd imaa <imanolvalenzuela03@gmail.com>
2022-09-06 00:01:53 -0300

    Contador con boton

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import ItemCount from "../ItemCount/ItemCount";
 import './itemListContainer.css';
 
 const ItemListContainer = (props) => {
+
+    const onAdd = (quantity) =>{
+        alert(`Has añadido ${quantity} unidades al carrito`)
+    }
+
     return(
         <div className="text-center">
             <h1>Batuk Originals</h1>
             <p> {props.season} <br /> <span className="fw-bold"> coming soon </span> </p>
+            <ItemCount initial={1} stock={5} onAdd={onAdd}/>
         </div>
     )
 }
